6dad0073d4afe1c6a2021803c585987f4dad5803
nick <jnicholastorres1@gmail.com>
2023-01-27 01:00:31 -0600

    Green Light    |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -13,42 +13,37 @@ class Cube:
         
         cubeList = list(self.cube)
         rotatedCubeList = cubeList[:]
-        #rotate front face
-        
+        #rotate front face      
         rotatedCubeList[FTL] = cubeList[FTR]
         rotatedCubeList[FML] = cubeList[FTM]
-        rotatedCubeList[FBL] = cubeList[FTL]
-        
+        rotatedCubeList[FBL] = cubeList[FTL]        
         rotatedCubeList[FTM] = cubeList[FMR]
         rotatedCubeList[FMM] = cubeList[FMM]
-        rotatedCubeList[FBM] = cubeList[FML]
-        
-        
+        rotatedCubeList[FBM] = cubeList[FML]       
         rotatedCubeList[FTR] = cubeList[FBR]
         rotatedCubeList[FMR] = cubeList[FBM]
         rotatedCubeList[FBR] = cubeList[FBL]
         
-        #rotate up to right
-        rotatedCubeList[RTL] = cubeList[UBL]
-        rotatedCubeList[RML] = cubeList[UBM]
-        rotatedCubeList[RBL] = cubeList[UBR]
+        #rotate up to left
+        rotatedCubeList[LTR] = cubeList[UBR]
+        rotatedCubeList[LMR] = cubeList[UBM]
+        rotatedCubeList[LBR] = cubeList[UBL]
         
-        #rotate bottom to left
+        #rotate bottom to right
+        rotatedCubeList[DTL] = cubeList[LTR]
+        rotatedCubeList[DTM] = cubeList[LMR]
+        rotatedCubeList[DTR] = cubeList[LBR]
         
-        rotatedCubeList[DTR] = cubeList[RTL]
-        rotatedCubeList[DTM] = cubeList[RML]
-        rotatedCubeList[DTL] = cubeList[RBL]
-        #rotate right to bottom
+        #rotate left to bottom   
+        rotatedCubeList[RTL] = cubeList[DTR]
+        rotatedCubeList[RML] = cubeList[DTM]
+        rotatedCubeList[RBL] = cubeList[DTL]
         
-        rotatedCubeList[LTR] = cubeList[DTL]
-        rotatedCubeList[LMR] = cubeList[DTM]
-        rotatedCubeList[LBR] = cubeList[DTR]
+        #rotate right to top       
+        rotatedCubeList[UBL] = cubeList[RTL]
+        rotatedCubeList[UBM] = cubeList[RML]
+        rotatedCubeList[UBR] = cubeList[RBL]
         
-        #rotate left to top
-        
-        rotatedCubeList[UBR] = cubeList[LTR]
-        rotatedCubeList[UBM] = cubeList[LMR]
-        rotatedCubeList[UBL] = cubeList[LBR]
         self.cube = "".join(rotatedCubeList)
         return self.cube
         
